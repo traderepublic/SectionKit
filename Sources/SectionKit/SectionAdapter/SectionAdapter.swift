16c96fdb7b19b63712ed47a9315be7ce849b5171
Niclas Kristek <niclas.kristek@traderepublic.com>
2020-05-06 12:25:44 +0200

    Add SectionAdapterDataSource prototype

--- Sources/SectionKit/SectionAdapter/SectionAdapter.swift
+++ Sources/SectionKit/SectionAdapter/SectionAdapter.swift
@@ -1,15 +1,69 @@
 import UIKit
 
 public protocol SectionAdapter: AnyObject {
+    associatedtype DataSource: AdapterDataSource
+    
     /// An object providing contextual information
     var collectionContext: CollectionContext { get }
     
     /// A delegate that receives callbacks from the `UIScrollView`
     var scrollViewDelegate: UIScrollViewDelegate? { get set }
     
-    /// The list of sections in the `UICollectionView`.
-    var sectionControllers: [SectionController] { get set }
+    /// The datasource of this `SectionAdapter` responsible for creating `SectionControllers`
+    var dataSource: DataSource { get set }
     
     /// If reordering should be allowed between different sections
     var allowReorderingBetweenDifferentSections: Bool { get set }
+    
+    /// Tells the `SectionAdapter` to query the `dataSource` when the data changed
+    func invalidateDataSource()
+}
+
+public class AnyAdapterDataSource {
+    
+}
+
+public protocol AdapterDataSource: AnyObject {
+    associatedtype SectionIdentifier: Hashable
+    
+    /// Returns objects for each section.
+    var objects: [AdapterObject<SectionIdentifier>] { get }
+    
+    /// Returns the `SectionController` for the provided object
+    func sectionController(with identifier: SectionIdentifier,
+                           items: [AnyHashable]) -> SectionController
+}
+
+public enum AdapterObject<SectionIdentifier: Hashable> {
+    case section(identifier: SectionIdentifier, items: [AnyHashable])
+}
+
+// MARK: - Sample implementation
+
+enum MySections: String {
+    case first
+    case second
+}
+
+class MyAdapterDataSource: AdapterDataSource {
+    var objects: [AdapterObject<MySections>] {
+        return [
+            .section(identifier: .first, items: ["1", "2"]),
+            .section(identifier: .second, items: ["3", "4"])
+        ]
+    }
+    
+    func sectionController(with identifier: SectionIdentifier,
+                           items: [AnyHashable]) -> SectionController {
+        switch identifier {
+        case .first:
+            let sectionController = ListSectionController<String>(id: identifier.rawValue)
+            sectionController.items = items.map { $0 as! String }
+            return sectionController
+        case .second:
+            let sectionController = ListSectionController<String>(id: identifier.rawValue)
+            sectionController.items = items.map { $0 as! String }
+            return sectionController
+        }
+    }
 }
