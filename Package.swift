31c9e4bc0925a94a44641484c26b122341e77897
Niclas Kristek <niclas.kristek@traderepublic.com>
2020-09-09 14:40:56 +0200

    Fix SwiftLint double reporting

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:5.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -9,28 +8,26 @@ let package = Package(
         .iOS(.v9)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "SectionKit",
-            targets: ["SectionKit"]),
-        .library(
-            name: "DiffingSectionKit",
-            targets: ["DiffingSectionKit"])
+        .library(name: "SectionKit", targets: ["SectionKit"]),
+        .library(name: "DiffingSectionKit", targets: ["DiffingSectionKit"])
     ],
     dependencies: [
         .package(url: "https://github.com/ra1028/DifferenceKit", from: "1.1.5")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "SectionKit",
-            dependencies: []),
-        .target(
-            name: "DiffingSectionKit",
-            dependencies: ["SectionKit", "DifferenceKit"]),
-        .testTarget(
-            name: "SectionKitTests",
-            dependencies: ["SectionKit"])
+        .target(name: "SectionKit",
+                path: "SectionKit/Sources"),
+        .testTarget(name: "SectionKitTests",
+                    dependencies: ["SectionKit"],
+                    path: "SectionKit/Tests"),
+        .target(name: "DiffingSectionKit",
+                dependencies: ["SectionKit", "DifferenceKit"],
+                path: "DiffingSectionKit/Sources"),
+        .testTarget(name: "DiffingSectionKitTests",
+                    dependencies: ["DiffingSectionKit"],
+                    path: "DiffingSectionKit/Tests")
+    ],
+    swiftLanguageVersions: [
+        .v5
     ]
 )
