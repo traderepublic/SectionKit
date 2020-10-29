491396c51995ee0e0fd2fb788a2fd7c7a574c3be
Niclas Kristek <niclas.kristek@traderepublic.com>
2020-10-29 14:32:34 +0100

    Remove unused convenience methods

--- SectionKit/Sources/Utility/IndexPath+IsValid.swift
+++ SectionKit/Sources/Utility/IndexPath+IsValid.swift
@@ -5,18 +5,4 @@ extension IndexPath {
     internal var isValid: Bool {
         count == 2 // precondition of the `section` property
     }
-
-    @inlinable
-    internal func isSectionIndexValid(for sections: [Any]) -> Bool {
-        count == 2 // precondition of the `section` property
-            && section >= 0
-            && section < sections.count
-    }
-
-    @inlinable
-    internal func isItemIndexValid(for items: [Any]) -> Bool {
-        count == 2 // precondition of the `item` property
-            && item >= 0
-            && item < items.count
-    }
 }
