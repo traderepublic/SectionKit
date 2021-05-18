97da7a64d7ade2c0cefa6eab1f242ab7f39d3245
Niclas Kristek <niclas.kristek@traderepublic.com>
2021-05-18 16:51:26 +0200

    Update examples for new API
    Signed-off-by: Niclas Kristek <niclas.kristek@traderepublic.com>

--- Example/SectionKitExample/Colors/ColorSectionController.swift
+++ Example/SectionKitExample/Colors/ColorSectionController.swift
@@ -6,17 +6,24 @@ final class ColorSectionController: ListSectionController<ColorViewModelType, UI
         model.colors
     }
 
-    override func cellForItem(at indexPath: SectionIndexPath) -> UICollectionViewCell {
-        let cell = context!.dequeueReusableCell(ColorCell.self, for: indexPath.indexInCollectionView)
+    override func cellForItem(
+        at indexPath: SectionIndexPath,
+        in context: CollectionViewContext
+    ) -> UICollectionViewCell {
+        let cell = context.dequeueReusableCell(ColorCell.self, for: indexPath.indexInCollectionView)
         cell.color = items[indexPath.indexInSectionController]
         return cell
     }
 
-    override func sizeForItem(at indexPath: SectionIndexPath, using layout: UICollectionViewLayout) -> CGSize {
-        CGSize(width: context!.containerSize.width, height: 50)
+    override func sizeForItem(
+        at indexPath: SectionIndexPath,
+        using layout: UICollectionViewLayout,
+        in context: CollectionViewContext
+    ) -> CGSize {
+        CGSize(width: context.containerSize.width, height: 50)
     }
 
-    override func didSelectItem(at indexPath: SectionIndexPath) {
+    override func didSelectItem(at indexPath: SectionIndexPath, in context: CollectionViewContext) {
         model.select(color: items[indexPath.indexInSectionController])
     }
 }
--- Example/SectionKitExample/ExampleList/ExampleListSectionController.swift
+++ Example/SectionKitExample/ExampleList/ExampleListSectionController.swift
@@ -6,42 +6,61 @@ final class ExampleListSectionController: ListSectionController<ExampleListSecti
         model.examples
     }
 
-    override func cellForItem(at indexPath: SectionIndexPath) -> UICollectionViewCell {
-        let cell = context!.dequeueReusableCell(ExampleCell.self, for: indexPath.indexInCollectionView)
+    override func cellForItem(
+        at indexPath: SectionIndexPath,
+        in context: CollectionViewContext
+    ) -> UICollectionViewCell {
+        let cell = context.dequeueReusableCell(ExampleCell.self, for: indexPath.indexInCollectionView)
         cell.configure(with: items[indexPath.indexInSectionController])
         return cell
     }
 
-    override func sizeForItem(at indexPath: SectionIndexPath, using layout: UICollectionViewLayout) -> CGSize {
-        CGSize(width: context!.containerSize.width - 48, height: 50)
+    override func sizeForItem(
+        at indexPath: SectionIndexPath,
+        using layout: UICollectionViewLayout,
+        in context: CollectionViewContext
+    ) -> CGSize {
+        CGSize(width: context.containerSize.width - 48, height: 50)
     }
 
-    override func didSelectItem(at indexPath: SectionIndexPath) {
+    override func didSelectItem(at indexPath: SectionIndexPath, in context: CollectionViewContext) {
         items[indexPath.indexInSectionController].didSelect()
     }
 
     // MARK: - Header
 
-    override func headerView(at indexPath: SectionIndexPath) -> UICollectionReusableView {
-        let headerView = context!.dequeueReusableHeaderView(ExampleListHeaderView.self, for: indexPath.indexInCollectionView)
+    override func headerView(
+        at indexPath: SectionIndexPath,
+        in context: CollectionViewContext
+    ) -> UICollectionReusableView {
+        let headerView = context.dequeueReusableHeaderView(ExampleListHeaderView.self, for: indexPath.indexInCollectionView)
         headerView.configure(with: model)
         return headerView
     }
 
-    override func referenceSizeForHeader(using layout: UICollectionViewLayout) -> CGSize {
-        CGSize(width: context!.containerSize.width, height: 50)
+    override func referenceSizeForHeader(
+        using layout: UICollectionViewLayout,
+        in context: CollectionViewContext
+    ) -> CGSize {
+        CGSize(width: context.containerSize.width, height: 50)
     }
 
     // MARK: - Footer
 
-    override func footerView(at indexPath: SectionIndexPath) -> UICollectionReusableView {
-        let footerView = context!.dequeueReusableFooterView(ExampleListFooterView.self, for: indexPath.indexInCollectionView)
+    override func footerView(
+        at indexPath: SectionIndexPath,
+        in context: CollectionViewContext
+    ) -> UICollectionReusableView {
+        let footerView = context.dequeueReusableFooterView(ExampleListFooterView.self, for: indexPath.indexInCollectionView)
         footerView.configure(with: model)
         return footerView
     }
 
-    override func referenceSizeForFooter(using layout: UICollectionViewLayout) -> CGSize {
+    override func referenceSizeForFooter(
+        using layout: UICollectionViewLayout,
+        in context: CollectionViewContext
+    ) -> CGSize {
         guard model.footer != nil else { return .zero }
-        return CGSize(width: context!.containerSize.width, height: 50)
+        return CGSize(width: context.containerSize.width, height: 50)
     }
 }
