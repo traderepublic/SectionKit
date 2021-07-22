1f34954909d23223afdc84666c1e8593e3ebfba7
Niclas Kristek <niclas.kristek@traderepublic.com>
2021-07-22 17:51:29 +0200

    Add file and line number parameters to ErrorHandling.on
    Signed-off-by: Niclas Kristek <niclas.kristek@traderepublic.com>

--- SectionKit/Sources/Error/ErrorHandling.swift
+++ SectionKit/Sources/Error/ErrorHandling.swift
@@ -6,7 +6,13 @@ public protocol ErrorHandling {
      A function that is called when an error is encountered.
      - Parameter error: The error that is encountered.
      */
-    func on(error: Error)
+    func on(error: @autoclosure () -> Error, file: StaticString, line: UInt)
+}
+
+extension ErrorHandling {
+    public func callAsFunction(error: @autoclosure () -> Error, file: StaticString = #file, line: UInt = #line) {
+        on(error: error(), file: file, line: line)
+    }
 }
 
 /// This error handler calls `assertionFailure` for every error that is encountered.
@@ -14,7 +20,7 @@ public struct AssertionFailureErrorHandler: ErrorHandling {
     public init() { }
 
     @inlinable
-    public func on(error: Error) {
-        assertionFailure(error.description)
+    public func on(error: @autoclosure () -> Error, file: StaticString, line: UInt) {
+        assertionFailure(error().description, file: file, line: line)
     }
 }
