2d5fcb035c5474ef7d36ecbbe26870a060f5c5fe
Niclas Kristek <niclas.kristek@traderepublic.com>
2021-07-23 13:54:00 +0200

    Add unit tests for `CollectionViewAdapter` conforming to `UICollectionViewDataSourcePrefetching` (#66)
    Signed-off-by: Niclas Kristek <niclas.kristek@traderepublic.com>

---
+++ SectionKit/Tests/CollectionViewAdapter/UICollectionViewDataSourcePrefetching/BaseCollectionViewAdapterUICollectionViewDataSourcePrefetchingTests.swift
@@ -0,0 +1,129 @@
+@testable import SectionKit
+import UIKit
+import XCTest
+
+internal class BaseCollectionViewAdapterUICollectionViewDataSourcePrefetchingTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+    }
+
+    internal func createCollectionView(
+        frame: CGRect = .zero,
+        collectionViewLayout layout: UICollectionViewLayout = UICollectionViewFlowLayout()
+    ) -> UICollectionView {
+        UICollectionView(frame: frame, collectionViewLayout: layout)
+    }
+
+    internal func createCollectionViewAdapter(
+        collectionView: UICollectionView,
+        sections: [Section] = [],
+        viewController: UIViewController? = nil,
+        scrollViewDelegate: UIScrollViewDelegate? = nil,
+        errorHandler: ErrorHandling = AssertionFailureErrorHandler()
+    ) throws -> CollectionViewAdapter & UICollectionViewDataSourcePrefetching {
+        fatalError("not implemented")
+    }
+
+    internal func testPrefetchItems() throws {
+        let testExpectation = expectation(description: "Should invoke datasource prefetching delegate")
+        let collectionView = createCollectionView()
+        let itemsToPrefetch = [IndexPath(item: 0, section: 0), IndexPath(item: 1, section: 0)]
+        let adapter = try createCollectionViewAdapter(
+            collectionView: collectionView,
+            sections: [
+                Section(id: "", model: "", controller: {
+                    let sectionController = MockSectionController()
+                    sectionController.dataSourcePrefetchingDelegate = {
+                        let dataSourcePrefetchingDelegate = MockSectionDataSourcePrefetchingDelegate()
+                        dataSourcePrefetchingDelegate._prefetchItems = { indexPaths, _ in
+                            XCTAssertEqual(indexPaths.map(\.indexInCollectionView), itemsToPrefetch)
+                            testExpectation.fulfill()
+                        }
+                        return dataSourcePrefetchingDelegate
+                    }()
+                    return sectionController
+                })
+            ]
+        )
+        adapter.collectionView(collectionView, prefetchItemsAt: itemsToPrefetch)
+        waitForExpectations(timeout: 1)
+    }
+
+    internal func testPrefetchItemsWithInvalidIndices() throws {
+        let testExpectation = expectation(description: "Should invoke datasource prefetching delegate")
+        let collectionView = createCollectionView()
+        let itemsToPrefetch = [IndexPath(item: 0, section: 0), IndexPath(item: 1, section: 0)]
+        let invalidIndices = [IndexPath(), IndexPath(item: 0, section: 1)]
+        let adapter = try createCollectionViewAdapter(
+            collectionView: collectionView,
+            sections: [
+                Section(id: "", model: "", controller: {
+                    let sectionController = MockSectionController()
+                    sectionController.dataSourcePrefetchingDelegate = {
+                        let dataSourcePrefetchingDelegate = MockSectionDataSourcePrefetchingDelegate()
+                        dataSourcePrefetchingDelegate._prefetchItems = { indexPaths, _ in
+                            XCTAssertEqual(indexPaths.map(\.indexInCollectionView), itemsToPrefetch)
+                            testExpectation.fulfill()
+                        }
+                        return dataSourcePrefetchingDelegate
+                    }()
+                    return sectionController
+                })
+            ]
+        )
+        adapter.collectionView(collectionView, prefetchItemsAt: itemsToPrefetch + invalidIndices)
+        waitForExpectations(timeout: 1)
+    }
+
+    internal func testCancelPrefetchingForItems() throws {
+        let testExpectation = expectation(description: "Should invoke datasource prefetching delegate")
+        let collectionView = createCollectionView()
+        let itemsToCancel = [IndexPath(item: 0, section: 0), IndexPath(item: 1, section: 0)]
+        let adapter = try createCollectionViewAdapter(
+            collectionView: collectionView,
+            sections: [
+                Section(id: "", model: "", controller: {
+                    let sectionController = MockSectionController()
+                    sectionController.dataSourcePrefetchingDelegate = {
+                        let dataSourcePrefetchingDelegate = MockSectionDataSourcePrefetchingDelegate()
+                        dataSourcePrefetchingDelegate._cancelPrefetchingForItems = { indexPaths, _ in
+                            XCTAssertEqual(indexPaths.map(\.indexInCollectionView), itemsToCancel)
+                            testExpectation.fulfill()
+                        }
+                        return dataSourcePrefetchingDelegate
+                    }()
+                    return sectionController
+                })
+            ]
+        )
+        adapter.collectionView?(collectionView, cancelPrefetchingForItemsAt: itemsToCancel)
+        waitForExpectations(timeout: 1)
+    }
+
+    internal func testCancelPrefetchingForItemsWithInvalidIndices() throws {
+        let testExpectation = expectation(description: "Should invoke datasource prefetching delegate")
+        let collectionView = createCollectionView()
+        let itemsToCancel = [IndexPath(item: 0, section: 0), IndexPath(item: 1, section: 0)]
+        let invalidIndices = [IndexPath(), IndexPath(item: 0, section: 1)]
+        let adapter = try createCollectionViewAdapter(
+            collectionView: collectionView,
+            sections: [
+                Section(id: "", model: "", controller: {
+                    let sectionController = MockSectionController()
+                    sectionController.dataSourcePrefetchingDelegate = {
+                        let dataSourcePrefetchingDelegate = MockSectionDataSourcePrefetchingDelegate()
+                        dataSourcePrefetchingDelegate._cancelPrefetchingForItems = { indexPaths, _ in
+                            XCTAssertEqual(indexPaths.map(\.indexInCollectionView), itemsToCancel)
+                            testExpectation.fulfill()
+                        }
+                        return dataSourcePrefetchingDelegate
+                    }()
+                    return sectionController
+                })
+            ]
+        )
+        adapter.collectionView?(collectionView, cancelPrefetchingForItemsAt: itemsToCancel + invalidIndices)
+        waitForExpectations(timeout: 1)
+    }
+}
---
+++ SectionKit/Tests/CollectionViewAdapter/UICollectionViewDataSourcePrefetching/ListCollectionViewAdapterUICollectionViewDataSourcePrefetchingTests.swift
@@ -0,0 +1,21 @@
+@testable import SectionKit
+import UIKit
+import XCTest
+
+internal final class ListCollectionViewAdapterUICollectionViewDataSourcePrefetchingTests: BaseCollectionViewAdapterUICollectionViewDataSourcePrefetchingTests {
+    override internal func createCollectionViewAdapter(
+        collectionView: UICollectionView,
+        sections: [Section] = [],
+        viewController: UIViewController? = nil,
+        scrollViewDelegate: UIScrollViewDelegate? = nil,
+        errorHandler: ErrorHandling = AssertionFailureErrorHandler()
+    ) throws -> CollectionViewAdapter & UICollectionViewDataSourcePrefetching {
+        ListCollectionViewAdapter(
+            collectionView: collectionView,
+            sections: sections,
+            viewController: viewController,
+            scrollViewDelegate: scrollViewDelegate,
+            errorHandler: errorHandler
+        )
+    }
+}
