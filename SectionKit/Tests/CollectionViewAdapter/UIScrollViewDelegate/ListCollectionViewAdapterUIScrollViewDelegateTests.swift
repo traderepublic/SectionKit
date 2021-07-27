454e7a86466526de3c73efdf98ba7ee0bba13e00
Niclas Kristek <niclas.kristek@traderepublic.com>
2021-07-27 11:04:26 +0200

    Add unittests for UIScrollViewDelegate
    Signed-off-by: Niclas Kristek <niclas.kristek@traderepublic.com>

---
+++ SectionKit/Tests/CollectionViewAdapter/UIScrollViewDelegate/ListCollectionViewAdapterUIScrollViewDelegateTests.swift
@@ -0,0 +1,21 @@
+@testable import SectionKit
+import UIKit
+import XCTest
+
+internal final class ListCollectionViewAdapterUIScrollViewDelegateTests: BaseCollectionViewAdapterUIScrollViewDelegateTests {
+    override internal func createCollectionViewAdapter(
+        collectionView: UICollectionView,
+        sections: [Section] = [],
+        viewController: UIViewController? = nil,
+        scrollViewDelegate: UIScrollViewDelegate? = nil,
+        errorHandler: ErrorHandling = AssertionFailureErrorHandler()
+    ) throws -> CollectionViewAdapter & UIScrollViewDelegate {
+        ListCollectionViewAdapter(
+            collectionView: collectionView,
+            sections: sections,
+            viewController: viewController,
+            scrollViewDelegate: scrollViewDelegate,
+            errorHandler: errorHandler
+        )
+    }
+}
