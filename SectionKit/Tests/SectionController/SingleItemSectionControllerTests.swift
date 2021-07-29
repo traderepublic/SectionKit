020cfa998ae91525f919477c625f9330759ac7ff
Niclas Kristek <niclas.kristek@traderepublic.com>
2021-07-29 11:20:14 +0200

    Add unittests for SingleItemSectionController
    Signed-off-by: Niclas Kristek <niclas.kristek@traderepublic.com>

---
+++ SectionKit/Tests/SectionController/SingleItemSectionControllerTests.swift
@@ -0,0 +1,118 @@
+@testable import SectionKit
+import XCTest
+
+internal final class SingleItemSectionControllerTests: XCTestCase {
+    internal func testDidUpdateModelWithValidTypeSetsModel() {
+        let sectionController = SingleItemSectionController<String, String>(model: "1")
+        sectionController.didUpdate(model: "2")
+        XCTAssertEqual(sectionController.model, "2")
+    }
+
+    internal func testDidUpdateModelWithInvalidType() {
+        let errorExpectation = expectation(description: "The errorHandler should be called")
+        let sectionController = SingleItemSectionController<String, String>(model: "1")
+        let context = MainCollectionViewContext(
+            viewController: nil,
+            collectionView: UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()),
+            errorHandler: MockErrorHandler { error in
+                guard case let .sectionControllerModelTypeMismatch(expected, actual) = error else {
+                    XCTFail("The error should be sectionControllerModelTypeMismatch")
+                    return
+                }
+                XCTAssert(expected == String.self)
+                XCTAssert(actual == Int.self)
+                errorExpectation.fulfill()
+            }
+        )
+        sectionController.context = context
+        sectionController.didUpdate(model: 2)
+        XCTAssertEqual(sectionController.model, "1")
+        waitForExpectations(timeout: 1)
+    }
+
+    internal func testInitDoesNotUpdateItemIfShouldNotUpdateItem() {
+        class TestSectionController: SingleItemSectionController<String, String> {
+            var itemsForModelExpectation: XCTestExpectation?
+
+            init(model: String, itemsForModelExpectation: XCTestExpectation?) {
+                self.itemsForModelExpectation = itemsForModelExpectation
+                super.init(model: model, areItemsEqual: ==)
+            }
+
+            override func shouldUpdateItem(afterModelChangedTo model: String) -> Bool {
+                false
+            }
+
+            override func item(for model: String) -> String? {
+                itemsForModelExpectation?.fulfill()
+                return nil
+            }
+        }
+        let itemsForModelExpectation = expectation(description: "Should not invoke item(for:)")
+        itemsForModelExpectation.fulfill()
+        let _ = TestSectionController(model: "1", itemsForModelExpectation: itemsForModelExpectation)
+        waitForExpectations(timeout: 1)
+    }
+
+    internal func testSettingModelDoesNotUpdateItemIfShouldNotUpdateItem() {
+        class TestSectionController: SingleItemSectionController<String, String> {
+            var itemsForModelExpectation: XCTestExpectation?
+
+            init(model: String, itemsForModelExpectation: XCTestExpectation?) {
+                self.itemsForModelExpectation = itemsForModelExpectation
+                super.init(model: model, areItemsEqual: ==)
+            }
+
+            override func shouldUpdateItem(afterModelChangedTo model: String) -> Bool {
+                false
+            }
+
+            override func item(for model: String) -> String? {
+                itemsForModelExpectation?.fulfill()
+                return nil
+            }
+        }
+        let itemsForModelExpectation = expectation(description: "Should not invoke item(for:)")
+        itemsForModelExpectation.fulfill()
+        let sectionController = TestSectionController(model: "1", itemsForModelExpectation: nil)
+        // set expectation after init so a possible bug in the init is not caught by this test
+        sectionController.itemsForModelExpectation = itemsForModelExpectation
+        sectionController.model = "2"
+        waitForExpectations(timeout: 1)
+    }
+
+    internal func testShouldUpdateItemDefaultsToTrue() {
+        let sectionController = SingleItemSectionController<String, String>(model: "1")
+        XCTAssert(sectionController.shouldUpdateItem(afterModelChangedTo: "2"))
+    }
+
+    internal func testItemForModelInvokesNotImplementedError() {
+        let errorExpectation = expectation(description: "The errorHandler should be called")
+        let sectionController = SingleItemSectionController<String, String>(model: "1")
+        let context = MainCollectionViewContext(
+            viewController: nil,
+            collectionView: UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()),
+            errorHandler: MockErrorHandler { error in
+                guard case .notImplemented = error else {
+                    XCTFail("The error should be notImplemented")
+                    return
+                }
+                errorExpectation.fulfill()
+            }
+        )
+        sectionController.context = context
+        XCTAssertEqual(sectionController.item(for: "2"), nil)
+        waitForExpectations(timeout: 1)
+    }
+
+    internal func testNumberOfItems() {
+        let sectionController = SingleItemSectionController<String, String>(model: "1")
+        sectionController.item = "1"
+        let context = MainCollectionViewContext(
+            viewController: nil,
+            collectionView: UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()),
+            errorHandler: MockErrorHandler { _ in }
+        )
+        XCTAssertEqual(sectionController.numberOfItems(in: context), 1)
+    }
+}
