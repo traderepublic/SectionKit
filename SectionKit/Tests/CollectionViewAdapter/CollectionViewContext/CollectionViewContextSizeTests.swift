7214a9161a6a4f5622a366c224a3184cfccf594d
Niclas Kristek <niclas.kristek@traderepublic.com>
2021-08-04 17:19:16 +0200

    Rename test method
    Signed-off-by: Niclas Kristek <niclas.kristek@traderepublic.com>

--- SectionKit/Tests/CollectionViewAdapter/CollectionViewContext/CollectionViewContextSizeTests.swift
+++ SectionKit/Tests/CollectionViewAdapter/CollectionViewContext/CollectionViewContextSizeTests.swift
@@ -13,7 +13,7 @@ internal final class CollectionViewContextSizeTests: XCTestCase {
         XCTAssertEqual(context.containerSize, rect.size)
     }
 
-    internal func testContainerInset() {
+    internal func testCustomContainerInset() {
         let insets = UIEdgeInsets(top: 1, left: 2, bottom: 4, right: 8)
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
         collectionView.contentInset = insets
