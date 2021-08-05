fdd2706eb9c635da44bc4900b27f6fa35cbec7df
Niclas Kristek <niclas.kristek@traderepublic.com>
2021-08-05 16:06:03 +0200

    Add unit tests for sectioncontroller default implementation
    Signed-off-by: Niclas Kristek <niclas.kristek@traderepublic.com>

---
+++ SectionKit/Tests/SectionController/ProtocolDefaultValuesSectionControllerTests.swift
@@ -0,0 +1,41 @@
+import SectionKit
+import XCTest
+
+internal final class ProtocolDefaultValuesSectionControllerTests: XCTestCase {
+    private func createSectionController() -> SectionController {
+        class DefaultSectionController: SectionController {
+            var context: CollectionViewContext?
+            var dataSource: SectionDataSource = MockSectionDataSource()
+            func didUpdate(model: Any) { }
+        }
+        return DefaultSectionController()
+    }
+
+    @available(iOS 10.0, *)
+    internal func testDataSourcePrefetchingDelegateIsNil() {
+        let sectionController = createSectionController()
+        XCTAssertNil(sectionController.dataSourcePrefetchingDelegate)
+    }
+
+    internal func testDelegateIsNil() {
+        let sectionController = createSectionController()
+        XCTAssertNil(sectionController.delegate)
+    }
+
+    internal func testFlowDelegateIsNil() {
+        let sectionController = createSectionController()
+        XCTAssertNil(sectionController.flowDelegate)
+    }
+
+    @available(iOS 11.0, *)
+    internal func testDragDelegateIsNil() {
+        let sectionController = createSectionController()
+        XCTAssertNil(sectionController.dragDelegate)
+    }
+
+    @available(iOS 11.0, *)
+    internal func testDropDelegateIsNil() {
+        let sectionController = createSectionController()
+        XCTAssertNil(sectionController.dropDelegate)
+    }
+}
