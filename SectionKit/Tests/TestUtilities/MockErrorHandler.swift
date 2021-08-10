8b425a43818cbfbd7cd792c626869ed6b68657d2
Niclas Kristek <niclas.kristek@traderepublic.com>
2021-08-10 09:24:57 +0200

    Improve wording of mock error handler
    Signed-off-by: Niclas Kristek <niclas.kristek@traderepublic.com>

--- SectionKit/Tests/TestUtilities/MockErrorHandler.swift
+++ SectionKit/Tests/TestUtilities/MockErrorHandler.swift
@@ -5,7 +5,7 @@ internal struct MockErrorHandler: ErrorHandling {
     internal var onError: (Error, Error.Severity) -> Void
 
     init(onError: @escaping (Error, Error.Severity) -> Void = { error, severity in
-        XCTFail("ErrorHandler received \(severity): \(error.localizedDescription)")
+        XCTFail("ErrorHandler received \(severity) error: \(error.localizedDescription)")
     }) {
         self.onError = onError
     }
