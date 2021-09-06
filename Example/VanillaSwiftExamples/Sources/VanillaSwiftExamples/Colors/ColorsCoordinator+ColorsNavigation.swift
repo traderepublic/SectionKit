4796231a2214ae7bb938643c14961c6c634b0d4d
Niclas Kristek <niclas.kristek@traderepublic.com>
2021-09-06 17:11:13 +0200

    Convert the colors example to MVC (#83)
    Signed-off-by: Niclas Kristek <niclas.kristek@traderepublic.com>

--- Example/VanillaSwiftExamples/Sources/VanillaSwiftExamples/Colors/ColorsCoordinator+ColorsNavigation.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-extension ColorsCoordinator: ColorsNavigation {
-    internal func show(color: UIColor) {
-        let viewController = UIViewController()
-        viewController.view.backgroundColor = color
-        navigationController.pushViewController(viewController, animated: true)
-    }
-}
